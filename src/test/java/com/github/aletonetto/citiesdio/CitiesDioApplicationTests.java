b03b95d858dd8a0c0691d30da7c2fc3e55c45c8d
aletonetto <aletonetto6@gmail.com>
2021-05-22 03:37:11 -0300

    Primeiro commit

---
+++ src/test/java/com/github/aletonetto/citiesdio/CitiesDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aletonetto.citiesdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
